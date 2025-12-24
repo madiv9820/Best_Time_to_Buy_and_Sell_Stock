7c943c9a1aa7e5c6758f7c7b090e517c8c3e8e99
madiv9820 <themathur901@gmail.com>
2025-12-25 00:47:29 +0530

    Cython wrapper for c++ solution class

---
+++ src/cpp_code/solution.pyx
@@ -0,0 +1,21 @@
+from libcpp.vector cimport vector
+
+cdef extern from "solution.hpp":
+    cdef cppclass Solution:
+        Solution() except + 
+        int maxProfit(vector[int]& prices)
+
+cdef class cppSolution:
+    cdef Solution* thisPtr
+    def __cinit__(self): self.thisPtr = new Solution()
+    def __dealloc__(self): del self.thisPtr
+    
+    def maxProfit(self, int[:] prices):
+        cdef vector[int] cpp_arr
+        cdef int price
+        
+        for price in prices:
+            cpp_arr.push_back(price)
+        
+        result = self.thisPtr.maxProfit(cpp_arr)
+        return result
